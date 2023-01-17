f8b0a3021c2f0e09ed771adba66ce1dd15c65a8b
J. Salvador Arias <jsalarias@gmail.com>
2023-01-17 12:57:03 -0300

    pixprob: implement pixel probabilities based on raster values

---
+++ pixprob/pixprob.go
@@ -0,0 +1,115 @@
+// Copyright © 2022 J. Salvador Arias <jsalarias@gmail.com>
+// All rights reserved.
+// Distributed under BSD2 license that can be found in the LICENSE file.
+
+// Package pixprob associates a pixelation raster value
+// with a probability for a pixel.
+package pixprob
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+
+	"golang.org/x/exp/slices"
+)
+
+// Pixel is the prior probability of a pixel
+// given a raster value.
+//
+// Each pixel is assumed to be independent
+// of all other pixels.
+type Pixel map[int]float64
+
+// Prior returns the prior probability
+// of a pixel for a given raster value.
+func (px Pixel) Prior(v int) float64 {
+	return px[v]
+}
+
+// Values return the raster values
+// that have a defined prior.
+func (px Pixel) Values() []int {
+	vs := make([]int, 0, len(px))
+	for v := range px {
+		vs = append(vs, v)
+	}
+	slices.Sort(vs)
+
+	return vs
+}
+
+// ReadTSV reads a TSV file used to define the prior probability values
+// for a given set of pixels values in a pixelation.
+//
+// The pixel prior file is a tab-delimited file
+// with the following columns:
+//
+//	-key	the value used as identifier
+//	-prior	the prior probability for a pixel with that value
+//
+// Any other columns,
+// will be ignored.
+// Here is an example of a pixel prior file:
+//
+//	key	prior	comment
+//	0	0.000000	deep ocean
+//	1	0.010000	oceanic plateaus
+//	2	0.050000	continental shelf
+//	3	0.950000	lowlands
+//	4	1.000000	highlands
+//	5	0.001000	ice sheets
+func ReadTSV(r io.Reader) (Pixel, error) {
+	tsv := csv.NewReader(r)
+	tsv.Comma = '\t'
+	tsv.Comment = '#'
+
+	head, err := tsv.Read()
+	if err != nil {
+		return nil, fmt.Errorf("while reading header: %v", err)
+	}
+	fields := make(map[string]int, len(head))
+	for i, h := range head {
+		h = strings.ToLower(h)
+		fields[h] = i
+	}
+	for _, h := range []string{"key", "prior"} {
+		if _, ok := fields[h]; !ok {
+			return nil, fmt.Errorf("expecting field %q", h)
+		}
+	}
+
+	p := Pixel{}
+	for {
+		row, err := tsv.Read()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		ln, _ := tsv.FieldPos(0)
+		if err != nil {
+			return nil, fmt.Errorf("on row %d: %v", ln, err)
+		}
+
+		f := "key"
+		k, err := strconv.Atoi(row[fields[f]])
+		if err != nil {
+			return nil, fmt.Errorf("on row %d: field %q: %v", ln, f, err)
+		}
+
+		f = "prior"
+		pp, err := strconv.ParseFloat(row[fields[f]], 64)
+		if err != nil {
+			return nil, fmt.Errorf("on row %d: field %q: %v", ln, f, err)
+		}
+		if pp < 0 || pp > 1 {
+			return nil, fmt.Errorf("on row %d: field %q: invalid prior value %.6f", ln, f, pp)
+		}
+
+		p[k] = pp
+	}
+
+	return p, nil
+}
---
+++ pixprob/pixprob_test.go
@@ -0,0 +1,53 @@
+// Copyright © 2022 J. Salvador Arias <jsalarias@gmail.com>
+// All rights reserved.
+// Distributed under BSD2 license that can be found in the LICENSE file.
+
+package pixprob_test
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/js-arias/earth/pixprob"
+)
+
+func TestReadPixel(t *testing.T) {
+	data := `
+key	prior	comment
+0	0.000000	deep ocean
+1	0.010000	oceanic plateaus
+2	0.050000	continental shelf
+3	0.950000	lowlands
+4	1.000000	highlands
+5	0.001000	ice sheets
+`
+
+	want := pixprob.Pixel{
+		0: 0,
+		1: 0.01,
+		2: 0.05,
+		3: 0.95,
+		4: 1,
+		5: 0.001,
+	}
+
+	p, err := pixprob.ReadTSV(strings.NewReader(data))
+	if err != nil {
+		t.Fatalf("unable to read data: %v", err)
+	}
+	if !reflect.DeepEqual(p, want) {
+		t.Errorf("got %v, want %v", p, want)
+	}
+
+	vs := []int{0, 1, 2, 3, 4, 5}
+	if g := p.Values(); !reflect.DeepEqual(g, vs) {
+		t.Errorf("values: got %v, want %v", g, vs)
+	}
+
+	for _, v := range vs {
+		if p.Prior(v) != want[v] {
+			t.Errorf("prior: value %d: got %.6f, want %.6f", v, p.Prior(v), want[v])
+		}
+	}
+}
